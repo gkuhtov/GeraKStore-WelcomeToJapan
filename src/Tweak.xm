4f98ec2657460ee6694b3623196365d0155f5759
gkuhtov <gkuhtov@users.noreply.github.com>
2026-09-05 18:45:13 +0800

    Replace risky hooks with constructor and UIApplicationDidFinishLaunchingNotification from original binary

--- src/Tweak.xm
+++ src/Tweak.xm
@@ -1,30 +1,12 @@
 #import <UIKit/UIKit.h>
-#import "WelcomeViewController.h"
+#import "WelcomeManager.h"
 
-%hook UIWindow
-
-- (void)makeKeyAndVisible {
-    %orig;
-
-    if ([[NSUserDefaults standardUserDefaults] boolForKey:@"GeraKWelcomeDismissed"]) {
-        return;
-    }
-
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.6 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            UIWindow *window = (UIWindow *)self;
-            if (window.rootViewController) {
-                WelcomeViewController *welcomeVC = [[WelcomeViewController alloc] init];
-                welcomeVC.view.frame = window.bounds;
-                welcomeVC.view.alpha = 0.0;
-                [window addSubview:welcomeVC.view];
-                [UIView animateWithDuration:0.4 animations:^{
-                    welcomeVC.view.alpha = 1.0;
-                }];
-            }
-        });
-    });
+__attribute__((constructor))
+static void WelcomeToJapanInit(void) {
+    [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidFinishLaunchingNotification
+                                                      object:nil
+                                                       queue:[NSOperationQueue mainQueue]
+                                                  usingBlock:^(NSNotification *note) {
+        [[WelcomeManager sharedManager] startWelcomeIfNeeded];
+    }];
 }
-
-%end
--- src/WelcomeManager.m
+++ src/WelcomeManager.m
@@ -3,6 +3,7 @@
 
 @interface WelcomeManager ()
 @property (nonatomic, assign) BOOL isPresenting;
+@property (nonatomic, assign) BOOL welcomeShownThisSession;
 @end
 
 @implementation WelcomeManager
@@ -20,55 +21,73 @@
     if ([[NSUserDefaults standardUserDefaults] boolForKey:@"com.gkuhtov.WelcomeToJapan.hasSeenWelcome"]) {
         return;
     }
-    if (self.isPresenting) {
+    if (self.welcomeShownThisSession || self.isPresenting) {
         return;
     }
-    [self presentWithRetryCount:0];
+    [self presentWelcomeWithRetry:0];
 }
 
-- (UIWindow *)findActiveKeyWindow {
-    for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
-        if (scene.activationState == UISceneActivationStateForegroundActive && [scene isKindOfClass:[UIWindowScene class]]) {
-            UIWindowScene *windowScene = (UIWindowScene *)scene;
-            for (UIWindow *win in windowScene.windows) {
-                if (win.isKeyWindow) {
-                    return win;
+- (UIViewController *)topViewControllerFrom:(UIViewController *)vc {
+    if ([vc isKindOfClass:[UINavigationController class]]) {
+        return [self topViewControllerFrom:[(UINavigationController *)vc visibleViewController]];
+    }
+    if ([vc isKindOfClass:[UITabBarController class]]) {
+        return [self topViewControllerFrom:[(UITabBarController *)vc selectedViewController]];
+    }
+    if (vc.presentedViewController) {
+        return [self topViewControllerFrom:vc.presentedViewController];
+    }
+    return vc;
+}
+
+- (void)presentWelcomeWithRetry:(NSInteger)retryCount {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.8 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        if ([UIApplication sharedApplication].applicationState != UIApplicationStateActive) {
+            if (retryCount < 20) {
+                [self presentWelcomeWithRetry:retryCount + 1];
+            }
+            return;
+        }
+
+        UIWindow *targetWindow = nil;
+        for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
+            if ([scene isKindOfClass:[UIWindowScene class]]) {
+                UIWindowScene *windowScene = (UIWindowScene *)scene;
+                for (UIWindow *win in windowScene.windows) {
+                    if (win.isKeyWindow || (win.rootViewController && !win.hidden)) {
+                        targetWindow = win;
+                        break;
+                    }
                 }
             }
-            if (windowScene.windows.count > 0) {
-                return windowScene.windows.firstObject;
-            }
+            if (targetWindow) break;
         }
-    }
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
-    for (UIWindow *win in [UIApplication sharedApplication].windows) {
-        if (win.isKeyWindow) {
-            return win;
+        if (!targetWindow) {
+            for (UIWindow *win in [UIApplication sharedApplication].windows) {
+                if (win.isKeyWindow || (win.rootViewController && !win.hidden)) {
+                    targetWindow = win;
+                    break;
+                }
+            }
         }
-    }
-    return [UIApplication sharedApplication].windows.firstObject;
 #pragma clang diagnostic pop
-}
 
-- (void)presentWithRetryCount:(NSInteger)retries {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIWindow *keyWindow = [self findActiveKeyWindow];
-
-        UIViewController *topVC = keyWindow.rootViewController;
-        while (topVC.presentedViewController) {
-            topVC = topVC.presentedViewController;
-        }
+        UIViewController *rootVC = targetWindow.rootViewController;
+        UIViewController *topVC = [self topViewControllerFrom:rootVC];
 
         if (!topVC || !topVC.isViewLoaded || topVC.view.window == nil) {
-            if (retries < 15) {
-                [self presentWithRetryCount:retries + 1];
+            if (retryCount < 20) {
+                [self presentWelcomeWithRetry:retryCount + 1];
             }
             return;
         }
 
         self.isPresenting = YES;
+        self.welcomeShownThisSession = YES;
+
         WelcomeViewController *welcomeVC = [[WelcomeViewController alloc] init];
         welcomeVC.modalPresentationStyle = UIModalPresentationOverFullScreen;
         welcomeVC.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
